2bd4d4b3427d0ec1a1cc2fdc789cf3d51eaddd84
JI-doo <awt0311@gmail.com>
2020-11-24 19:58:59 +0900

    [Modify] URL

--- SW-Hackerton/SW-Hackerton/Source/Networking/MrsURL.swift
+++ SW-Hackerton/SW-Hackerton/Source/Networking/MrsURL.swift
@@ -12,16 +12,23 @@ import Alamofire
 enum MrsURL {
     case LogIn
     case info
+    case requestList
+    case acceptanceRefusal
 
+    
     func path() -> String {
         switch self {
         case .LogIn:
             return "/teacher_login"
         case .info:
             return "/teacher_info"
-
+        case .requestList:
+            return "/book_list"
+        case .acceptanceRefusal:
+            return "/room_accept"
         }
     }
+    
     func header() -> HTTPHeaders? {
         switch self {
         default:
